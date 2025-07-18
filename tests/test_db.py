65ffa6c4a4a00087b65771d48a3c9988e9c86a2a
Pavlo Kostianov <pasha.k460@gmail.com>
2025-07-18 16:35:09 +0100

    Add database tests to test adding and removing timeline posts to/from the database

---
+++ tests/test_db.py
@@ -0,0 +1,51 @@
+import unittest
+from peewee import *
+
+from app import TimelinePost
+
+MODELS = [TimelinePost]
+
+# use an in-memory SQLite for tests.
+test_db = SqliteDatabase(':memory:')
+
+class TestTimelinePost(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of
+        # all models, we do not need to recursively bind dependencies.
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
+
+        # Close connection to db.
+        test_db.close()
+
+    def test_timeline_post(self):
+        first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+        assert first_post.id == 1
+        second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+        assert second_post.id == 2
+
+        # Get timeline posts and assert that they are correct
+        got_first_post = TimelinePost.get(TimelinePost.id == 1)
+        assert got_first_post.name == 'John Doe'
+        assert got_first_post.email == 'john@example.com'
+        assert got_first_post.content == 'Hello world, I\'m John!'
+        got_second_post = TimelinePost.get(TimelinePost.id == 2)
+        assert got_second_post.name == 'Jane Doe'
+        assert got_second_post.email == 'jane@example.com'
+        assert got_second_post.content == 'Hello world, I\'m Jane!'
+
+        # Delete posts and check if they were deleted
+        first_post.delete_instance()
+        with self.assertRaises(TimelinePost.DoesNotExist):
+            TimelinePost.get(TimelinePost.id == 1)
+        second_post.delete_instance()
+        with self.assertRaises(TimelinePost.DoesNotExist):
+            TimelinePost.get(TimelinePost.id == 2)
